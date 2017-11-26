2da1b11079a4d4e4dc4ea4e18da60d536dca92f8
irawan09 <arykurniadiirawan@yahoo.co.id>
2017-11-26 15:59:39 +0800

    baru

--- app/src/main/java/com/laelektronik/user/portaldesa/Fragment/VideoFragment.java
+++ app/src/main/java/com/laelektronik/user/portaldesa/Fragment/VideoFragment.java
@@ -25,7 +25,7 @@ public class VideoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_lokasi_kegiatan, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_video, container, false);
 
         String pesan = getArguments().getString("pesan");
         final int id = getArguments().getInt("id");
