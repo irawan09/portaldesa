4e12ea01927ff6178d49427644d3071cb7af56ba
irawan09 <arykurniadiirawan@yahoo.co.id>
2017-11-28 17:58:05 +0800

    baru

--- app/src/main/java/com/laelektronik/user/portaldesa/Fragment/BiodataFragment.java
+++ app/src/main/java/com/laelektronik/user/portaldesa/Fragment/BiodataFragment.java
@@ -1,11 +1,15 @@
 package com.laelektronik.user.portaldesa.Fragment;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.laelektronik.user.portaldesa.Activity.MainActivity;
 import com.laelektronik.user.portaldesa.R;
@@ -15,6 +19,12 @@ import com.laelektronik.user.portaldesa.R;
  */
 public class BiodataFragment extends Fragment {
 
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    String username;
+
+    ImageView imageView;
+    TextView namauser, nama, lahir, alamat, kelamin, instansi, telepon, email;
 
     public BiodataFragment() {
         // Required empty public constructor
@@ -33,8 +43,21 @@ public class BiodataFragment extends Fragment {
         ((MainActivity) getActivity()).setTitleActionBar(pesan);
         ((MainActivity) getActivity()).setSelectedItem(id);
 
-        // Inflate the layout for this fragment
+        imageView = (ImageView) rootView.findViewById(R.id.imageView);
+        namauser = (TextView) rootView.findViewById(R.id.namauser2);
+        nama = (TextView) rootView.findViewById(R.id.nama);
+        lahir = (TextView) rootView.findViewById(R.id.lahir);
+        alamat = (TextView) rootView.findViewById(R.id.alamat);
+        kelamin = (TextView) rootView.findViewById(R.id.kelamin);
+        instansi = (TextView) rootView.findViewById(R.id.instansi);
+        telepon = (TextView) rootView.findViewById(R.id.telepon);
+        email = (TextView) rootView.findViewById(R.id.email);
 
+        preferences = getActivity().getSharedPreferences("MY_PREF", Context.MODE_PRIVATE);
+        username = preferences.getString("username", null);
+        namauser.setText(username);
+
+        // Inflate the layout for this fragment
         return rootView;
     }
 }
