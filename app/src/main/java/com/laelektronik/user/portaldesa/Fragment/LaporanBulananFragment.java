3eb94928b8511043c5bb28bafabc98949493e89d
irawan09 <arykurniadiirawan@yahoo.co.id>
2017-12-20 03:00:17 +0800

    galery

--- app/src/main/java/com/laelektronik/user/portaldesa/Fragment/LaporanBulananFragment.java
+++ app/src/main/java/com/laelektronik/user/portaldesa/Fragment/LaporanBulananFragment.java
@@ -45,11 +45,7 @@ import static android.app.Activity.RESULT_OK;
  */
 public class LaporanBulananFragment extends Fragment {
 
-    private Uri mImageCaptureUri;
-    private ImageView mImageView;
 
-    private static final int PICK_FROM_CAMERA = 1;
-    private static final int PICK_FROM_FILE = 2;
 
     public LaporanBulananFragment() {
         // Required empty public constructor
@@ -61,90 +57,15 @@ public class LaporanBulananFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_laporan_bulanan, container, false);
 
-        final String [] items           = new String [] {"From Camera", "From SD Card"};
-        ArrayAdapter<String> adapter  = new ArrayAdapter<String>(getContext(), android.R.layout.select_dialog_item,items);
-        AlertDialog.Builder builder     = new AlertDialog.Builder(getContext());
-        builder.setTitle("Select Image");
-        builder.setAdapter( adapter, new DialogInterface.OnClickListener() {
-            public void onClick( DialogInterface dialog, int item ) {
-                if (item == 0) {
-                    Intent intent    = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    File file        = new File(Environment.getExternalStorageDirectory(),
-                            "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
-                    mImageCaptureUri = Uri.fromFile(file);
 
-                    try {
-                        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
-                        intent.putExtra("return-data", true);
-
-                        startActivityForResult(intent, PICK_FROM_CAMERA);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                    dialog.cancel();
-                } else {
-                    Intent intent = new Intent();
-
-                    intent.setType("image/*");
-                    intent.setAction(Intent.ACTION_GET_CONTENT);
-
-                    startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
-                }
-            }
-        } );
-
-        final AlertDialog dialog = builder.create();
-
-        mImageView = (ImageView) rootview.findViewById(R.id.iv_pic);
-
-        ((Button) rootview.findViewById(R.id.btn_choose)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.show();
-            }
-        });
 
         // Inflate the layout for this fragment
         return rootview;
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode != RESULT_OK) return;
 
-        Bitmap bitmap   = null;
-        String path     = "";
 
-        if (requestCode == PICK_FROM_FILE) {
-            mImageCaptureUri = data.getData();
-            path = getRealPathFromURI(mImageCaptureUri); //from Gallery
 
-            if (path == null)
-                path = mImageCaptureUri.getPath(); //from File Manager
-
-            if (path != null)
-                bitmap  = BitmapFactory.decodeFile(path);
-        } else {
-            path    = mImageCaptureUri.getPath();
-            bitmap  = BitmapFactory.decodeFile(path);
-        }
-
-        mImageView.setImageBitmap(bitmap);
-    }
-
-    public String getRealPathFromURI(Uri contentUri) {
-        String [] proj      = {MediaStore.Images.Media.DATA};
-        Cursor cursor       = getActivity().managedQuery( contentUri, proj, null, null,null);
-
-        if (cursor == null) return null;
-
-        int column_index    = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-
-        cursor.moveToFirst();
-
-        return cursor.getString(column_index);
-    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
--- app/src/main/java/com/laelektronik/user/portaldesa/Fragment/LaporanMingguanFragment.java
+++ app/src/main/java/com/laelektronik/user/portaldesa/Fragment/LaporanMingguanFragment.java
@@ -3,7 +3,14 @@ package com.laelektronik.user.portaldesa.Fragment;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,15 +18,27 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.laelektronik.user.portaldesa.R;
+
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LaporanMingguanFragment extends Fragment {
 
+    private Uri mImageCaptureUri;
+    private ImageView mImageView;
+
+    private static final int PICK_FROM_CAMERA = 1;
+    private static final int PICK_FROM_FILE = 2;
 
     public LaporanMingguanFragment() {
         // Required empty public constructor
@@ -31,9 +50,88 @@ public class LaporanMingguanFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_laporan_mingguan, container, false);
 
+        final String [] items           = new String [] {"From Camera", "From SD Card"};
+        ArrayAdapter<String> adapter  = new ArrayAdapter<String>(getContext(), android.R.layout.select_dialog_item,items);
+        AlertDialog.Builder builder     = new AlertDialog.Builder(getContext());
+        builder.setTitle("Select Image");
+        builder.setAdapter( adapter, new DialogInterface.OnClickListener() {
+            public void onClick( DialogInterface dialog, int item ) {
+                if (item == 0) {
+                    Intent intent    = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File file        = new File(Environment.getExternalStorageDirectory(),
+                            "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg");
+                    mImageCaptureUri = Uri.fromFile(file);
+
+                    try {
+                        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+                        intent.putExtra("return-data", true);
+
+                        startActivityForResult(intent, PICK_FROM_CAMERA);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    dialog.cancel();
+                } else {
+                    Intent intent = new Intent();
+
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                    startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
+                }
+            }
+        } );
+
+        final AlertDialog dialog = builder.create();
+
+        mImageView = (ImageView) rootview.findViewById(R.id.iv_pic);
+
+        ((Button) rootview.findViewById(R.id.btn_choose)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
 
         // Inflate the layout for this fragment
         return rootview;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) return;
+
+        Bitmap bitmap   = null;
+        String path     = "";
+
+        if (requestCode == PICK_FROM_FILE) {
+            mImageCaptureUri = data.getData();
+            path = getRealPathFromURI(mImageCaptureUri); //from Gallery
+
+            if (path == null)
+                path = mImageCaptureUri.getPath(); //from File Manager
+
+            if (path != null)
+                bitmap  = BitmapFactory.decodeFile(path);
+        } else {
+            path    = mImageCaptureUri.getPath();
+            bitmap  = BitmapFactory.decodeFile(path);
+        }
+
+        mImageView.setImageBitmap(bitmap);
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String [] proj      = {MediaStore.Images.Media.DATA};
+        Cursor cursor       = getActivity().managedQuery( contentUri, proj, null, null,null);
+
+        if (cursor == null) return null;
+
+        int column_index    = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+        cursor.moveToFirst();
+
+        return cursor.getString(column_index);
     }
 
     @Override
